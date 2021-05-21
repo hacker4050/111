6c563748077eafd3f159e45947ba9b13fca0757a
hacker4050 <77092460+hacker4050@users.noreply.github.com>
2021-05-21 21:15:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+function setup(){
+  canvas = createCanvas(displayWidth - 8, displayHeight-112);  
+}
+
+function draw(){
+  background("white");
+    fill("black");
+    textSize(15);text(" PUT PASSWORD AND GET AMAZING PRIZES ",displayWidth/2-600, displayHeight/2-300);
+    textSize(15);text(" TO GET HELP CLICK ABOVE LINK ",displayWidth/2-600, displayHeight/2-250);
+    textSize(15);text(" THERES MORE SUPRISES WAITING FOR YOU ",displayWidth/2-600, displayHeight/2-200);
+    textSize(15);text(" THERES MORE SUPRISES WAITING FOR YOU ",displayWidth/2-600, displayHeight/2-150);
+    textSize(15);text(" THERES MORE SUPRISES WAITING FOR YOU ",displayWidth/2-600, displayHeight/2-100);
+    textSize(15);text(" PUT PASSWORD AND GET AMAZING PRIZES ",displayWidth/2-600, displayHeight/2-50);
+    textSize(15);text(" TO GET HELP CLICK ABOVE LINK ",displayWidth/2-600, displayHeight/2+0);
+    textSize(15);text(" THERES MORE SUPRISES WAITING FOR YOU ",displayWidth/2-600, displayHeight/2+50);
+    textSize(15);text(" THERES MORE SUPRISES WAITING FOR YOU ",displayWidth/2-600, displayHeight/2+100);
+    textSize(15);text(" THERES MORE SUPRISES WAITING FOR YOU ",displayWidth/2-600, displayHeight/2+150);
+    
+}
